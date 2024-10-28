355cc19bf7b689fecfa026897c5a4cb9962df1a1
kordeliakoo <e0970535@u.nus.edu>
2024-10-28 20:57:26 +0800

    utilised User contract

--- contracts/ResumeReviews.sol
+++ contracts/ResumeReviews.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./User.sol";
+
 contract ResumeReviews {
+    User public userContract;
+
     struct WorkExperience {
         string jobTitle;
         string description;
@@ -23,18 +27,26 @@ contract ResumeReviews {
         uint256 reputationScore;
     }
 
-    mapping(address => UserProfile) public users;
-    
-    event WorkExperienceAdded(address indexed user, string jobTitle);
-    event ReviewSubmitted(address indexed reviewer, address indexed reviewee, uint8 rating);
-    
+    mapping(uint256 => UserProfile) public users; // Changed to use userId instead of address
+
+    event WorkExperienceAdded(uint256 indexed userId, string jobTitle); // Changed to use userId instead of address
+    event ReviewSubmitted(uint256 indexed reviewerId, uint256 indexed revieweeId, uint8 rating); // Changed to use userId instead of address
+
+    constructor(address userAddress) {
+        userContract = User(userAddress);  // Initialize the User contract
+    }
+
     function addWorkExperience(
-        string memory jobTitle, 
-        string memory description, 
-        uint256 startDate, 
-        uint256 endDate, 
+        uint256 userId,
+        string memory jobTitle,
+        string memory description,
+        uint256 startDate,
+        uint256 endDate,
         string[] memory skills
     ) public {
+        require(userContract.isUserType(userId, User.UserType.Freelancer), "Only freelancers can add work experience.");
+        require(userContract.getAddressFromUserId(userId) == msg.sender, "Unauthorized access.");
+
         WorkExperience memory newExperience = WorkExperience({
             jobTitle: jobTitle,
             description: description,
@@ -42,13 +54,20 @@ contract ResumeReviews {
             endDate: endDate,
             skills: skills
         });
-        
-        users[msg.sender].workHistory.push(newExperience);
+
+        users[userId].workHistory.push(newExperience);
         emit WorkExperienceAdded(msg.sender, jobTitle);
     }
 
-    function addReview(address reviewee, uint8 rating, string memory comment) public {
+    function addReview(
+        uint256 reviewerId,
+        uint256 revieweeId,
+        uint8 rating,
+        string memory comment
+    ) public {
         require(rating > 0 && rating <= 5, "Rating must be between 1 and 5");
+        require(userContract.isUserType(reviewerId, User.UserType.Client), "Only clients can add reviews.");
+        require(userContract.getAddressFromUserId(reviewerId) == msg.sender, "Unauthorized access");
 
         Review memory newReview = Review({
             reviewer: msg.sender,
@@ -56,33 +75,37 @@ contract ResumeReviews {
             comment: comment,
             timestamp: block.timestamp
         });
-        
-        users[reviewee].reviews.push(newReview);
-        updateReputation(reviewee);
 
-        emit ReviewSubmitted(msg.sender, reviewee, rating);
+        users[revieweeId].reviews.push(newReview);
+        updateReputation(revieweeId);
+
+        emit ReviewSubmitted(reviewerId, revieweeId, rating);
     }
 
-    function updateReputation(address user) internal {
+    function updateReputation(uint256 userId) internal {
         uint256 totalScore = 0;
-        Review[] memory userReviews = users[user].reviews;
-        
+        Review[] memory userReviews = users[userId].reviews;
+
         for (uint256 i = 0; i < userReviews.length; i++) {
             totalScore += userReviews[i].rating;
         }
-        
-        users[user].reputationScore = userReviews.length > 0 ? totalScore / userReviews.length : 0;
+
+        users[userId].reputationScore = userReviews.length > 0
+            ? totalScore / userReviews.length
+            : 0;
     }
 
-    function getWorkHistory(address user) public view returns (WorkExperience[] memory) {
-        return users[user].workHistory;
+    function getWorkHistory(
+        uint256 userId
+    ) public view returns (WorkExperience[] memory) {
+        return users[userId].workHistory;
     }
 
-    function getReviews(address user) public view returns (Review[] memory) {
-        return users[user].reviews;
+    function getReviews(uint256 userId) public view returns (Review[] memory) {
+        return users[userId].reviews;
     }
 
-    function getReputationScore(address user) public view returns (uint256) {
-        return users[user].reputationScore;
+    function getReputationScore(uint256 userId) public view returns (uint256) {
+        return users[userId].reputationScore;
     }
 }
